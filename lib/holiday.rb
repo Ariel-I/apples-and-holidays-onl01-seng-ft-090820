4a467a15602266382d9364ae8c374c85c82fd378
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 20:56:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,13 +18,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
      }
    }
    
-   holiday_hash.each do |season, holiday|
-     holiday.each do |attribute, value|
-       if attribute == :fourth_of_july
-           puts [1]
-         end
-       end 
-    end
+   holiday_hash[:summer][:fourth_of_july][1]
      
   # return the second element in the 4th of July array
 end
