1f58d93761fe9dcb29f9de809b96b27970823b0d
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 22:20:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -141,12 +141,15 @@ def all_supplies_in_holidays(holiday_hash)
    }
   
   
-  holiday_hash.each do |season, holiday|
+  
+
+ holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    puts " #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    
+   end 
   end
-end
   
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
