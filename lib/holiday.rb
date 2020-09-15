25880b67cddc0721905f79ec4e634b5b60060867
Ariel Ibarra <arielibarra39@gmail.com>
2020-09-15 21:54:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -127,7 +127,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,holidays|
     holidays.each do |holiday,supplies|
       puts "#{holiday}: #{supplies.join(",")}"
-    end 
+    
    end 
   end
   
